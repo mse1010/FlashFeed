249662a6ae2e1a4e064f4853924bc6da414f3903
paulcnl <paul.canarelli.2504@gmail.com>
2024-10-04 16:17:17 +0200

    final

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const container = document.querySelector(".cardcontainer");
 
 container.addEventListener("scroll", () => {
   articles.forEach((article) => {
-    console.log(article);
     const rect = article.getBoundingClientRect();
     const containerRect = container.getBoundingClientRect();
-    console.log("scroll");
     // Vérifie si l'élément est au centre
-    const isCentered =
-      rect.left >= containerRect.left + containerRect.width / 4 &&
-      rect.right <= containerRect.right - containerRect.width / 4;
+
+    let isCentered = null;
+    if (window.innerWidth >= 1000) {
+      isCentered =
+        rect.left >= containerRect.left + containerRect.width / 4 &&
+        rect.right <= containerRect.right - containerRect.width / 4;
+    } else {
+      isCentered =
+        rect.top >= containerRect.top + containerRect.width / 4 &&
+        rect.bottom <= containerRect.bottom - containerRect.width / 4;
+    }
+
     if (isCentered) {
       article.classList.remove("grey");
     } else {
